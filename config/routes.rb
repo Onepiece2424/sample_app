d000624642ecf1682864aaa4ab72e8da711a22af
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-12-25 08:11:40 +0900

    トップページに「ログイン」「ログアウト」「新規登録」機能のルーティングとその機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+  root 'pages#index'
   get 'pages/show'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
