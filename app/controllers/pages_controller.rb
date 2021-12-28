dace06cd89991c6efa3fe19f7f9f8036a5922eaf
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-12-28 20:19:06 +0900

    インデントの追加

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, only: [:show,:new]
+  before_action :authenticate_user!, only: [:show, :new]
 
   def index
     @pages = Page.all
