14363932a2fd1689326854d97f935b8c1ac732c3
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-12-29 12:50:56 +0900

    レスポンスにdate,worry,reason,approachの有無を確認するテストの作成

--- spec/request/pages_spec.rb
+++ spec/request/pages_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Page', type: :request do
-  let(:page) { create(:page) }
+  let!(:page) { create(:page) }
 
   before do
     get pages_path
@@ -14,6 +14,22 @@ RSpec.describe 'Page', type: :request do
 
     it 'ページタイトルが存在していること' do
       assert_select "title", "Worries List"
+    end
+
+    it 'レスポンスにdateが含まれること' do
+      expect(response.body). to include page.date
+    end
+
+    it 'レスポンスにworryが含まれること' do
+      expect(response.body). to include page.worry
+    end
+
+    it 'レスポンスにreasonが含まれること' do
+      expect(response.body). to include page.reason
+    end
+
+    it 'レスポンスにapproachが含まれること' do
+      expect(response.body). to include page.approach
     end
   end
 end
