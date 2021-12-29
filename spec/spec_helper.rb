42444ca8fa88949e42324702012e4860cf8ccc8d
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-12-29 18:50:27 +0900

    require 'capybara/rspec'の追加

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,7 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'capybara/rspec'
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -90,4 +91,5 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   Kernel.srand config.seed
+  config.include Capybara::DSL
 end
