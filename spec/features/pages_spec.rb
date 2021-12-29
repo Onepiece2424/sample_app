27bae56045f1572d65b4494fa734855762e78682
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-12-29 18:22:21 +0900

    不要な部分の削除

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'Page feature', type: :feature do
-  given!(:page) { create(:page) }
+  given(:page) { create(:page) }
 
   background do
     visit pages_path
@@ -9,9 +9,7 @@ RSpec.feature 'Page feature', type: :feature do
 
   feature 'Worry一覧ページ' do
     scenario 'dateが表示されること' do
-      # within(:css, 'div.container') do
-        expect(page).to have_content page.date
-      # end
+      expect(page).to have_content page.date
     end
   end
 end
