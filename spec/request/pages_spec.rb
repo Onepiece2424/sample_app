ef3f79d4aaeae73bf288dd84189af00e2a2b35e4
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-12-29 11:01:35 +0900

    pageのrequest specの作成

---
+++ spec/request/pages_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Page', type: :request  do
+  let(:page) { create(:page) }
+  binding.pry
+  before do
+    get pages_path
+  end
+
+  describe 'Worry一覧ページ' do
+    it 'レスポンスが返ってくること' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'ページタイトルが存在していること' do
+      assert_select "title", "Worries List"
+    end
+  end
+end
