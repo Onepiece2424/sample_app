b7a73a91f379d8e95be6aaf569a5950dcae9a7f3
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-12-29 18:17:08 +0900

    page,feature specの作成

---
+++ spec/features/pages_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature 'Page feature', type: :feature do
+  given!(:page) { create(:page) }
+
+  background do
+    visit pages_path
+  end
+
+  feature 'Worry一覧ページ' do
+    scenario 'dateが表示されること' do
+      # within(:css, 'div.container') do
+        expect(page).to have_content page.date
+      # end
+    end
+  end
+end
