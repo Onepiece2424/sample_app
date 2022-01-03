0cfc2f71b32e38c96cfe01f6cb9e8f4ac8ad1d00
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-01-03 11:02:25 +0900

    descriptionページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  root 'pages#index'
-  resources :pages
+  root 'pages#description'
+  resources :pages do
+    collection do
+      get :description
+    end
+  end
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
