77426a96e49e2e2467bd9198eb8bbee538a80b41
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-01-04 12:07:42 +0900

    Pageモデルにcategoryカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_04_023211) do
+ActiveRecord::Schema.define(version: 2022_01_04_030513) do
 
   create_table "pages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_01_04_023211) do
     t.text "worry"
     t.text "thinking"
     t.text "approach"
+    t.string "category"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
