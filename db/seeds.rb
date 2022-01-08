359efe7b784667aa05d9d086cee6899a1b2772da
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-01-08 18:08:08 +0900

    seedデータの導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Page.create(date:'2022-01-01', category:'お金', worry:'所持金が3000円をきりそう', thinking:'電気代が4000円だから足りない。', approach:'銀行で3000円くらいおろそう')
