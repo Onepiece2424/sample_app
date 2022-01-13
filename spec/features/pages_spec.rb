14264067dca50fccba0dcf0761a53546ada17c05
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-01-14 06:37:14 +0900

    ログイン後のメッセージ表示テストの追加

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -9,5 +9,6 @@ RSpec.feature "Pages", type: :feature do
     fill_in "メールアドレス", with: user.email
     fill_in "パスワード", with: user.password
     click_button "ログイン"
+    expect(page).to have_content 'ログインしました'
   end
 end
