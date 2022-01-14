a934f88f5c67d49446fefa5d0beea602aead6217
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-01-15 05:10:22 +0900

    ページ内に特定の文字列が表示されていることを検証するテストの追加

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -2,13 +2,26 @@ require 'rails_helper'
 
 RSpec.feature "Pages", type: :feature do
   given(:user) { create(:user) }
+  given!(:p) { create(:page) }
 
-  scenario "ログイン機能" do
+  background do
     visit root_path
     click_link "ログイン"
     fill_in "メールアドレス", with: user.email
     fill_in "パスワード", with: user.password
     click_button "ログイン"
+  end
+
+  scenario "ログイン機能" do
     expect(page).to have_content 'ログインしました'
+  end
+
+  scenario "新規Worryの作成" do
+    visit pages_path
+    expect(page).to have_content p.category
+    expect(page).to have_content p.date
+    expect(page).to have_content p.thinking
+    expect(page).to have_content p.worry
+    expect(page).to have_content p.approach
   end
 end
